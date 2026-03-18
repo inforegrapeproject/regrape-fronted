c95c0a4ab16f0ad56953970d0f59e983f21d06e4
inforegrapeproject <info.regrapeproject@gmail.com>
2026-03-18 15:43:46 +0200

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+ /** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        border: "hsl(var(--border))",
+      }
+    }
+  },
+  plugins: [require("tailwindcss-animate")],
+}
